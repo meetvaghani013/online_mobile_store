d18e5a1c67a3a35b9d60bb2f767b852ecf514d0f
Meet Vaghani <mr.official1304@gmail.com>
2026-03-07 12:26:01 +0530

    Switch database URI to use SQLite from environment

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ app.secret_key = "secret123"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "database.db")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "database.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
